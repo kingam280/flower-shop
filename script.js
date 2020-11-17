9ca9ad435d9cde6a5d8b012b593e297d6bd72dd9
Kinga Mamak <kingam280@gmail.com>
2020-11-17 17:00:11 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const showMore = () => {
+    const gallery = document.querySelector(".menu")
+    if (gallery.style.display === "block") {
+        gallery.style.display = "none"
+    } else {
+        gallery.style.display = "block"
+    }
+    
+
+}
